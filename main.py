5a95ab3a74e7df69859b85cb4494ace26c63791b
Luka Ramon Tinarelli <lukaramontinarelli47@gmail.com>
2025-10-06 14:01:24 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from sudoku import imprimir_tablero#, solucionar_sudoku, comprobar_tablero
-
-tablero = [["-" if input() == "" else input() for _ in range(9)] for _ in range(9)]
-
-imprimir_tablero(tablero)
